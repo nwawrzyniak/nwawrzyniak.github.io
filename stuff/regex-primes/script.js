f1c6f2b37eb37920a5d045dd19d216048dc2979f
nwawrzyniak <nico.wawrzyniak@live.de>
2024-11-06 20:32:59 +0100

    Improved coloring

--- stuff/regex-primes/script.js
+++ stuff/regex-primes/script.js
@@ -1,13 +1,16 @@
 const noJavascript = document.querySelector(".no-javascript");
 const requiresJavascript = document.querySelector(".requires-javascript");
-const regex = /^.?$|^(..+?)\1+$/;
+const regex = /^.?$|^(..+?)\1+$/; /* The magic regex. Explanation here: https://www.youtube.com/watch?v=5vbk0TwkokM */
 const colorCodedRegexInnerHTML = "<span class=\"red\">/</span><span class=\"yellow\">^</span><span class=\"red\">.</span><span class=\"orange\">?</span><span class=\"yellow\">$</span><span>|</span><span class=\"yellow\">^</span><span class=\"brown\">(</span><span class=\"red\">..</span><span class=\"orange\">+</span><span class=\"orange\">?</span><span class=\"brown\">)</span><span class=\"blue\">\\1</span><span class=\"orange\">+</span><span class=\"yellow\">$</span><span class=\"red\">/</span>";
 const inputField = document.querySelector("#input");
 const outputRegexPre = document.querySelector("#output-regex-pre");
 const outputRegex = document.querySelector("#output-regex");
 const outputRegexPost = document.querySelector("#output-regex-post");
 const outputHuman = document.querySelector("#output-human-readable-text");
-inputField.value = Math.ceil(Math.random() * 1000);
+do {
+  inputField.value = Math.ceil(Math.random() * 1000); /* Sets a random initial number between 1 and 1000 to check */
+} while (inputField.value % 2 == 0 || inputField.value % 5 == 0); /* Ensures that the number is "interesting" (that it is odd and does not end with a 5) */
+
 noJavascript.style.display = "none";
 requiresJavascript.style.display = "block";
 
@@ -18,10 +21,10 @@ function checkPrime() {
     outputRegex.innerHTML = comparator;
     outputRegexPost.innerHTML = "or any other string that consists of " + n + " times the same character.";
     if (!regex.test(comparator)) {
-        outputRegexPre.innerHTML = "The regular expression " + colorCodedRegexInnerHTML + " does <span class=\"red\">not</span> match the string";
+        outputRegexPre.innerHTML = "The regular expression " + colorCodedRegexInnerHTML + " <span class=\"prime\">does not match</span> the string";
         outputHuman.innerHTML = "Therefore, " + n + " is a <span class=\"prime\">prime</span> number.";
     } else {
-        outputRegexPre.innerHTML = "The regular expression " + colorCodedRegexInnerHTML + " does match the string";
+        outputRegexPre.innerHTML = "The regular expression " + colorCodedRegexInnerHTML + " <span class=\"composite\">does match</span> the string";
         outputHuman.innerHTML = "Therefore, " + n + " is a <span class=\"composite\">composite</span> number.";
     }
 }
