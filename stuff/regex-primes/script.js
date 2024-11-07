e12bc5eb9dbb0d022b373518133da30f03b81c3f
nwawrzyniak <nico.wawrzyniak@live.de>
2024-11-07 21:50:44 +0100

    Optimizations

--- stuff/regex-primes/script.js
+++ stuff/regex-primes/script.js
@@ -1,26 +1,29 @@
 const noJavascript = document.querySelector(".no-javascript");
 const requiresJavascript = document.querySelector(".requires-javascript");
 const regex = /^.?$|^(..+?)\1+$/; /* The magic regex. Explanation here: https://www.youtube.com/watch?v=5vbk0TwkokM */
-const colorCodedRegexInnerHTML = "<span class=\"red\">/</span><span class=\"yellow\">^</span><span class=\"red\">.</span><span class=\"orange\">?</span><span class=\"yellow\">$</span><span>|</span><span class=\"yellow\">^</span><span class=\"brown\">(</span><span class=\"red\">..</span><span class=\"orange\">+</span><span class=\"orange\">?</span><span class=\"brown\">)</span><span class=\"blue\">\\1</span><span class=\"orange\">+</span><span class=\"yellow\">$</span><span class=\"red\">/</span>";
+const colorCodedRegexInnerHTML = "<div class=\"colored-regex-box\"><span class=\"red\">/</span><span class=\"yellow\">^</span><span class=\"red\">.</span><span class=\"orange\">?</span><span class=\"yellow\">$</span><span>|</span><span class=\"yellow\">^</span><span class=\"brown\">(</span><span class=\"red\">..</span><span class=\"orange\">+</span><span class=\"orange\">?</span><span class=\"brown\">)</span><span class=\"blue\">\\1</span><span class=\"orange\">+</span><span class=\"yellow\">$</span><span class=\"red\">/</span></div>";
 const inputField = document.querySelector("#input");
 const outputRegexPre = document.querySelector("#output-regex-pre");
 const outputRegex = document.querySelector("#output-regex");
 const outputRegexPost = document.querySelector("#output-regex-post");
 const outputHuman = document.querySelector("#output-human-readable-text");
+let number, lastDigit;
 do {
-  inputField.value = Math.ceil(Math.random() * 1000); /* Sets a random initial number between 1 and 1000 to check */
-} while (inputField.value % 2 == 0 || inputField.value % 10 == 5); /* Ensures that the number is "interesting" (that it is odd and does not end with a 5). The second part could simply be written as inputField.value % 5 == 0 but that would recheck numbers that end in a zero aswell. */
+    number = Math.ceil(Math.random() * 1000); /* Sets a random initial number between 1 and 1000 to check */
+    lastDigit = number % 10;
+} while (lastDigit != 1 && lastDigit != 3 && lastDigit != 7 && lastDigit != 9); /* Ensures that the number is "interesting" (that it ends in 1, 3, 7 or 9) */
+inputField.value = number;
 
 noJavascript.style.display = "none";
-requiresJavascript.style.display = "block";
+requiresJavascript.style.display = "flex";
 
 function checkPrime() {
     let n = inputField.value;
     if (n == "") n = 0;
-    const comparator = '0'.repeat(n);
+    const comparator = '0'.repeat(n); /* Creates a string that contains any character exactly n times. We use '0', but it does not matter. */
     outputRegex.innerHTML = comparator;
     outputRegexPost.innerHTML = "or any other string that consists of " + n + " times the same character.";
-    if (regex.test(comparator)) {
+    if (regex.test(comparator)) { /* Compares the regex against the comparator string */
         outputRegexPre.innerHTML = "The regular expression " + colorCodedRegexInnerHTML + " <span class=\"composite\">does match</span> the string";
         outputHuman.innerHTML = "Therefore, " + n + " is a <span class=\"composite\">composite</span> number.";
     } else {
